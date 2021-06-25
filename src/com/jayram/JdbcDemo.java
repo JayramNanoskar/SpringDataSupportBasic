037be0a42f707914678ddeb6979445c16fa6be06
JayramNanoskar <jayram.n978@gmail.com>
2021-06-25 20:01:27 +0530

    using Hibernate with Spring

--- src/com/jayram/JdbcDemo.java
+++ src/com/jayram/JdbcDemo.java
@@ -2,7 +2,9 @@ package com.jayram;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+import com.jayram.dao.HibernateDaoImpl;
 import com.jayram.dao.JdbcDaoImpl;
 import com.jayram.dao.JdbcDaoSupportImpl;
 import com.jayram.model.Circle;
@@ -15,9 +17,11 @@ public class JdbcDemo {
 		System.out.println(dao.getCircleForId(1).getName());
 		dao.insert(new Circle(3, "Third Circle"));
 		System.out.println(dao.getAllCircles().size());
-		dao.createTriangleTable();
+//		dao.createTriangleTable();
 		JdbcDaoSupportImpl daObj = context.getBean("jdbcDaoSupportImpl", JdbcDaoSupportImpl.class);
 		System.out.println(daObj.getCircleCount());
+		HibernateDaoImpl daObject = context.getBean("hibernateDaoImpl", HibernateDaoImpl.class);
+		System.out.println(daObject.getCircleCount());
 	}
 
 }
